f22164e69945fdaf8c76a7c36ba4c93f4196aea9
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-15 21:06:45 +0500

    Add project files.

---
+++ EnnheritanceDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnnheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello VS2022");
+            Console.ReadLine();
+        }
+    }
+}
